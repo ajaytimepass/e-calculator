f0b43cd9d6a57800dac396c7392e83c0e8b32840
ajaytimepass <64046061+ajaytimepass@users.noreply.github.com>
2020-07-22 21:02:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,35 +14,4 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 
 let output = document.getElementById('output');
 let input = document.getElementById('input');
-function sound() {
-   var soundwav = new Audio();
-  soundwav.src = "../audio/button-09.wav";
-  var soundmp3 = new Audio();
-  soundmp3.src = "../audio/button-09.mp3";
-  soundmp3.play();
-  soundwav.play();
-}
-function calculate(data) {
-  
-  switch(data){
 
-    case 'AC':
-      input.innerHTML="";
-      output.innerHTML="0";
-      sound();
-      break;
-      case 'C':
-        input.innerHTML=input.innerHTML.substr(0,input.innerHTML.length -1);
-        output.innerHTML=  math.evaluate(input.innerHTML) === undefined ? 0:math.evaluate(input.innerHTML);
-        sound();
-        break;
-      case '=':
-        output.innerHTML = math.evaluate(input.innerHTML);
-        sound();
-        break;
-    default:
-        input.innerHTML +=data;
-        sound();
-        break;
-  }
-}
