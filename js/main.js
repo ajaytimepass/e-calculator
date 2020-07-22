fc49b6bb9fa11805bd945f1ab3ab199e9a7a3581
BathaniAjay <61774100+BathaniAjay@users.noreply.github.com>
2020-07-22 18:15:29 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+let output = document.getElementById('output');
+let input = document.getElementById('input');
+function sound() {
+  var sound = new Audio("../audio/button-09.wav");
+  sound.play();
+}
+function calculate(data) {
+  
+  switch(data){
+
+    case 'AC':
+      input.innerHTML="";
+      output.innerHTML="0";
+      sound();
+      break;
+      case 'C':
+        input.innerHTML=input.innerHTML.substr(0,input.innerHTML.length -1);
+        output.innerHTML=  math.evaluate(input.innerHTML) === undefined ? 0:math.evaluate(input.innerHTML);
+        sound();
+        break;
+      case '=':
+        output.innerHTML = math.evaluate(input.innerHTML);
+        sound();
+        break;
+    default:
+        input.innerHTML +=data;
+        sound();
+        break;
+  }
+}
